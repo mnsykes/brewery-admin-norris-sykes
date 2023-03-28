42db91937ea155b78d2533c56e6aa05be194e87e
Matthew Sykes <61014119+mnsykes@users.noreply.github.com>
2023-03-28 07:34:30 -0400

    changes to multiple pages

--- public/index.js
+++ public/index.js
@@ -7,6 +7,32 @@ async function removeRequest(e) {
 	window.location.replace("/requests");
 }
 
-for (const btn of document.querySelectorAll(".action-btn")) {
+for (const btn of document.querySelectorAll(".delete-btn")) {
 	btn.onclick = removeRequest;
+}
+
+async function approveRequest(e) {
+	const requestId = e.target.getAttribute("data-request-id");
+	console.log(requestId);
+	await fetch(`/api/requests/${requestId}`, {
+		method: "put"
+	});
+	window.location.replace("/requests");
+}
+
+for (const btn of document.querySelectorAll(".approve-btn")) {
+	btn.onclick = approveRequest;
+}
+
+async function removeEmployee(e) {
+	const employeeId = e.target.getAttribute("data-request-id");
+
+	await fetch(`/api/employees/${employeeId}`, {
+		method: "delete"
+	});
+	window.location.replace("/employees");
+}
+
+for (const btn of document.querySelectorAll(".emp-action-btn")) {
+	btn.onclick = removeEmployee;
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,22 +3,119 @@ const db = require("../db");
 const path = require("path");
 
 router.get("/", async (req, res) => {
-	res.render("index", { heading: "Brewery Admin Tool", title: "Brewery Admin" });
+	const data = {
+		loggedIn: req.session.loggedIn,
+		heading: "Login"
+	};
+	res.render("index", { data });
+});
+
+router.get("/login", async (req, res) => {
+	res.render("login", { heading: "Login", title: "Brewery Admin" });
+});
+
+router.get("/dashboard", async (req, res) => {
+	const data = {
+		loggedIn: req.session.loggedIn,
+		heading: "Dashboard",
+		title: "toot | dashboard"
+	};
+
+	const pages = [
+		{
+			name: "Style Search",
+			bgColor: "search-bg_dark",
+			btnColor: "search-bg_light",
+			route: "/stylesearch"
+		},
+		{
+			name: "Tap Plan",
+			bgColor: "tapplan-bg_dark",
+			btnColor: "tapplan-bg_light",
+			route: "/tapplan"
+		},
+		{
+			name: "Requests",
+			bgColor: "requests-bg_dark",
+			btnColor: "requests-bg_light",
+			route: "/requests"
+		},
+		{
+			name: "Employees",
+			bgColor: "employees-bg_dark",
+			btnColor: "employees-bg_light",
+			route: "/employees"
+		}
+	];
+	res.render("dashboard", {
+		page: pages,
+		loggedIn: req.session.loggedIn,
+		heading: "Login",
+		title: "Brewery",
+		data
+	});
+});
+
+router.get("/employees", async (req, res) => {
+	const [roles] = await db.query(`SELECT * FROM roles`);
+	const [employees] = await db.query(`
+		SELECT 
+		employees.id,
+		employees.first_name,
+		employees.last_name,
+		roles.*
+		FROM employees JOIN roles ON employees.role_id = roles.id
+	`);
+
+	res.render("employees", {
+		role: roles,
+		employee: employees,
+		headerBg: "employees-bg_dark",
+		loggedIn: req.session.loggedIn
+	});
 });
 
 router.get("/stylesearch", async (req, res) => {
-	res.render("stylesearch", { headerBg: "search-bg_dark", beerimg: "/images/pils.jpeg" });
+	const data = {
+		loggedIn: req.session.loggedIn,
+		heading: "Style Search",
+		headerBg: "search-bg_dark",
+		beerimg: "/images/pils.jpeg",
+		title: "toot | style search"
+	};
+
+	res.render("stylesearch", data);
 });
 
 router.get("/tapplan", async (req, res) => {
-	res.render("tapplan", { headerBg: "tapplan-bg_dark" });
+	res.render("tapplan", {
+		headerBg: "tapplan-bg_dark",
+		loggedIn: req.session.loggedIn,
+		arrow: "arrow fa-sharp fa-solid fa-arrow-left fa-2xl"
+	});
 });
 
 router.get("/requests", async (req, res) => {
-	const [requests] = await db.query(`SELECT * FROM requests`);
-	const data = { request: requests, headerBg: "requests-bg_dark" };
+	const [requests] = await db.query(`
+		SELECT req.*, CONCAT_WS(' ', emp.first_name, emp.last_name) AS requestor
+		FROM requests req
+		LEFT JOIN employees emp ON req.requestor_id = emp.id
+	`);
 
-	res.render("requests", data);
+	const data = {
+		request: requests,
+		heading: "Dashboard",
+		title: "toot | dashboard",
+		headerBg: "requests-bg_dark",
+		loggedIn: req.session.loggedIn
+	};
+
+	res.render("requests", {
+		request: requests,
+		headerBg: "requests-bg_dark",
+		loggedIn: req.session.loggedIn,
+		data
+	});
 });
 
 module.exports = router;
