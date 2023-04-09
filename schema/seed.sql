94d052a652a90d9747a804b88144da06f52b1722
daniellanorris <daniellanorris@ufl.edu>
2023-04-09 17:42:15 -0400

    changes

--- schema/seed.sql
+++ schema/seed.sql
@@ -6,5 +6,3 @@ VALUES
     ("Brewhouse"),
     ("Tap Room");
 
-INSERT INTO employees (first_name, last_name, role_id,username, password)
-VALUES ("daniella", "norris","Management","banana", "$2b$10$/1UfZvXYrToAGa5mO1IbfesmFQJ42WIlI60swCyafhzrB/2KcD1RK");
